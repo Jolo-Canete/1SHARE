235ff046403a7982f517ff440afa2c1a3e44d04d
Jazleen <125132931+septimojazleenpaula29@users.noreply.github.com>
2024-03-11 07:07:32 +0000

    ASD

--- htdocs/test.php
+++ htdocs/test.php
@@ -16,9 +16,6 @@
 
   <!--- Style --->
   <style>
-    .dropdown:hover .dropdown-menu {
-      box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15);
-    }
     .sidebar {
       width: 280px;
       position: fixed;
@@ -38,14 +35,13 @@
     }
 
     .sidebar a:hover {
-      padding-left: 20px;
+      padding-left: 14px;
       background-color: #495057;
     }
 
     .navbar {
       z-index: 1031;
     }
-    
   </style>
 
 </head>
@@ -69,90 +65,74 @@
             </form>
 
             <div class="text-end">
-              <!--- Add content --->
+              <div class="bi-bell-fill text-white"></div>
             </div>
           </div>
         </div>
       </div>
     </div>
   </header>
-  
-<main>
-  <div class="sidebar">
-    <div class="d-flex flex-column flex-shrink-0 p-3 text-white" style="width: 280px;">
-      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-        <img src="picture/logo.png" alt="I S H A R E logo" style="height: 30px;">
-        <span class="fs-4 ms-lg-3">I S H A R E</span>
-      </a>
-      <hr>
-      <ul class="nav nav-pills flex-column mb-auto">
-        <li class="nav-item">
-          <a href="#" class="nav-link active" aria-current="page">
-            <div class="bi-house-door" width="16" height="16">
-              <span class="fs-8 ms-lg-2">Home</span>
-            </div>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="nav-link text-white">
-            <div class="bi-person-circle" width="16" height="16">
-              <span class="fs-8 ms-lg-2">Profile</span>
-            </div>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="nav-link text-white">
-            <div class="bi-box" width="16" height="16">
-              <span class="fs-8 ms-lg-2">Inventory</span>
-            </div>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="nav-link text-white">
-            <div class="bi-card-checklist" width="16" height="16">
-              <span class="fs-8 ms-lg-2">Request Approval</span>
-            </div>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="nav-link text-white">
-            <div class="bi-cart" width="16" height="16">
-              <span class="fs-8 ms-lg-2">Cart</span>
-            </div>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="nav-link text-white">
-            <div class="bi-speedometer2" width="16" height="16">
-              <span class="fs-8 ms-lg-2">Dashboard</span>
-            </div>   
-          </a>
-        </li>
-        <li>
-          <a href="#" class="nav-link text-white">
-            <div class="bi-gear" width="16" height="16">
-              <span class="fs-8 ms-lg-2">Settings</span>
-            </div>
-          </a>
-        </li>
-      </ul>
-      <hr>
-      <div class="dropdown">
-        <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-          <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
-          <strong>mdo</strong>
+
+  <main>
+    <div class="sidebar">
+      <div class="d-flex flex-column flex-shrink-0 p-3 text-white" style="width: 280px;">
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+          <img src="picture/logo.png" alt="I S H A R E logo" style="height: 30px;">
+          <span class="fs-4 ms-lg-3">I S H A R E</span>
         </a>
-        <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-          <li><a class="dropdown-item" href="#">New project...</a></li>
-          <li><a class="dropdown-item" href="#">Settings</a></li>
-          <li><a class="dropdown-item" href="#">Profile</a></li>
-          <li><hr class="dropdown-divider"></li>
-          <li><a class="dropdown-item" href="#">Sign out</a></li>
+        <hr>
+        <ul class="nav nav-pills flex-column mb-auto">
+          <li class="nav-item">
+            <a href="home.php" class="nav-link text-white" aria-current="page">
+              <div class="bi-house-door" width="16" height="16">
+                <span class="fs-8 ms-lg-2">Home</span>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="profile.php" class="nav-link text-white">
+              <div class="bi-person-circle" width="16" height="16">
+                <span class="fs-8 ms-lg-2">Profile</span>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="additem.php" class="nav-link text-white">
+              <div class="bi-box" width="16" height="16">
+                <span class="fs-8 ms-lg-2">Inventory</span>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="request.php" class="nav-link text-white">
+              <div class="bi-card-checklist" width="16" height="16">
+                <span class="fs-8 ms-lg-2">Request Approval</span>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="nav-link text-white">
+              <div class="bi-cart" width="16" height="16">
+                <span class="fs-8 ms-lg-2">Cart</span>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="dashboard.php" class="nav-link text-white">
+              <div class="bi-speedometer2" width="16" height="16">
+                <span class="fs-8 ms-lg-2">Dashboard</span>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="nav-link text-white">
+              <div class="bi-gear" width="16" height="16">
+                <span class="fs-8 ms-lg-2">Settings</span>
+              </div>
+            </a>
+          </li>
         </ul>
-      </div>
-    </div>
-  </div>
-</main>
+  </main>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
