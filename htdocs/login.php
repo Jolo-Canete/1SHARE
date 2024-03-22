2930055c21590d16a3a2330fe00555a75363e8b0
kumikiyodNgSaging <118452258+Nipo-Cyrus@users.noreply.github.com>
2024-03-22 00:06:19 +0000

    done input data [still buggy]

--- htdocs/login.php
+++ htdocs/login.php
@@ -1,3 +1,4 @@
+<?php ob_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -26,6 +27,7 @@
     </head>
 
     <body>
+        <?php include "1db.php"; ?>
         <header>
             <nav class="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
                 <div class="container">
@@ -50,11 +52,12 @@
                     <div class="card shadow p-3 mb-5 bg-body rounded-4">
                         <div class="card-body">
                             <div class="mb-3">
+                            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
                                 <!--- Login --->
                                 <label for="email_address">Email Address</label>
                                 <div class="input-group flex-nowrap">
                                     <span class="input-group-text" id="addon-wrapping">@</span>
-                                    <input type="text" class="form-control" placeholder="Email Address or Mobile Number" aria-label="Email Address or Mobile Number" aria-describedby="addon-wrapping">
+                                    <input type="text" class="form-control" name="email" placeholder="Email Address or Mobile Number" aria-label="Email Address or Mobile Number" aria-describedby="addon-wrapping">
 
                                 </div>
                                 <div class="mb-3"></div>
@@ -63,11 +66,11 @@
                                     <span class="input-group-text" id="addon-wrapping">
                                         <div class="bi-person-fill"></div>
                                     </span>
-                                    <input type="text" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="addon-wrapping">
+                                    <input type="text" class="form-control" name="password" placeholder="Password" aria-label="Password" aria-describedby="addon-wrapping">
                                 </div>
                                 <div class="mb-3"></div>
                                 <div class="d-grid gap-2">
-                                    <button id="loginButton" class="btn btn-primary" type="submit" value="login">Login</button>
+                                    <button id="loginButton" class="btn btn-primary" type="submit" name="login" value="login">Login</button>
                                     <script>
                                         // Function to redirect to the specified link when the button is clicked
                                         document.getElementById('loginButton').addEventListener('click', function() {
@@ -97,58 +100,58 @@
                                                     <div class="col">
                                                         <div class="mb-3">
                                                             <label for="first_name" class="form-label">First Name</label>
-                                                            <input type="text" class="form-control" placeholder="First name" id="first_name" aria-label="First name">
+                                                            <input type="text" class="form-control" placeholder="First name" id="first_name" name="first_name" aria-label="First name">
                                                         </div>
                                                     </div>
                                                     <div class="col">
                                                         <label for="middle_name" class="form-label">Middle Name</label>
-                                                        <input type="text" class="form-control" placeholder="Middle name" id="middle_name" aria-label="Middle name">
+                                                        <input type="text" class="form-control" placeholder="Middle name" id="middle_name" name="middle_name" aria-label="Middle name">
                                                     </div>
                                                     <div class="col">
                                                         <label for="last_name" class="form-label">Last Name</label>
-                                                        <input type="text" class="form-control" placeholder="Last name" id="last_name" aria-label="Last name">
+                                                        <input type="text" class="form-control" placeholder="Last name" id="last_name" name="last_name" aria-label="Last name">
                                                     </div>
                                                 </div>
                                                 <div class="row">
                                                     <div class="col">
                                                         <div class="mb-3">
                                                             <label for="purok" class="form-label">Purok</label>
-                                                            <input type="text" class="form-control" id="purok" placeholder="Enter purok" id="purok" aria-label="Enter purok">
+                                                            <input type="text" class="form-control" id="purok" placeholder="Enter purok" id="purok" name="purok" aria-label="Enter purok">
                                                         </div>
                                                     </div>
                                                     <div class="col">
                                                         <label for="zone" class="form-label">Zone</label>
-                                                        <input type="text" class="form-control" id="zone" placeholder="Enter zone" aria-label="Enter zone">
+                                                        <input type="text" class="form-control" id="zone" name="zone" placeholder="Enter zone" aria-label="Enter zone">
                                                     </div>
                                                     <div class="mb-3">
                                                         <label for="mobile_number" class="form-label">Mobile Number</label>
-                                                        <input type="text" class="form-control" id="mobile_number" placeholder="Enter mobile number" aria-label="Enter mobile number">
+                                                        <input type="text" class="form-control" id="mobile_number" name="mobile_number" placeholder="Enter mobile number" aria-label="Enter mobile number">
                                                     </div>
                                                     <div class="mb-3">
                                                         <label for="email_address">Email Address</label>
                                                         <div class="input-group flex-nowrap">
                                                             <span class="input-group-text" id="addon-wrapping">@</span>
-                                                            <input type="text" class="form-control" placeholder="Email Address or Mobile Number" aria-label="Email Address or Mobile Number" aria-describedby="addon-wrapping">
+                                                            <input type="text" class="form-control" placeholder="Email Address or Mobile Number" name="email" aria-label="Email Address or Mobile Number" aria-describedby="addon-wrapping">
                                                         </div>
                                                         <div class="mb-3">
                                                             <label for="new_password" class="form-label">New Password</label>
-                                                            <input type="password" class="form-control" id="new_password" placeholder="Create a new password" aria-label="Create a new password">
+                                                            <input type="password" class="form-control" id="new_password" name="password1" placeholder="Create a new password" aria-label="Create a new password">
                                                         </div>
                                                         <div class="mb-3">
                                                             <label for="confirm_new_password" class="form-label">Confirm New Password</label>
-                                                            <input type="password" class="form-control" id="confirm_new_password" placeholder="Confirm new password" aria-label="Confirm new password">
+                                                            <input type="password" class="form-control" id="confirm_new_password" name="password2" placeholder="Confirm new password" aria-label="Confirm new password">
                                                         </div>
                                                     </div>
                                                 </div>
                                                 <div class="modal-footer">
-                                                    <button class="btn btn-success d-grid gap-2 col-6 mx-auto" type="submit" value="sign_up">Sign Up</button>
+                                                    <button class="btn btn-success d-grid gap-2 col-6 mx-auto" name="signup" type="submit" value="sign_up">Sign Up</button>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
 
                                 </div>
-
+                            </form>
 
                                 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
                                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
@@ -158,36 +161,85 @@
     //  Login System
     // Check errors
     ini_set('display_errors', 1);
-    // Check login
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // check email and password
-        $email = $_POST['username'];
-        $password = $_POST['password'];
+        // Form Submission
 
-        // Query Sql to check if the email and password exist in the database
-        $sql = "SELECT * FROM users WHERE username AND password = '$username' AND password = '$password'";
-        $result = $conn->query($sql);
-
-        // Check the inputs
-        if ($result->num_rows == 1) {
-            // Username and password are correct, set session variables
-            $_SESSION['Loggedin'] = true;
-            $_SESSION['username'] = $username;
-
-            // Grant access the homepage
-            header("Location: home.php");
-            exit;
-        } else {
-            // username and/or password is incorrect
-            $error = "Ïnvalid username or password";
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (isset($_POST['signup'])) {
+                // Gather the Input data
+                $first_name = $_POST['first_name'];
+                $middle_name = $_POST['middle_name'];
+                $last_name = $_POST['last_name'];
+                $purok = $_POST['purok'];
+                $zone = $_POST['zone'];
+                $mobile_number = $_POST['mobile_number'];
+                $email = $_POST['email'];
+                $password1 = $_POST['password1'];
+                $password2 = $_POST['password2'];
+            
+                // Capture the correct time and date
+                $dateJoined = date('Y-m-d H:i:s');
+            
+                // Check if password matched
+                if ($password1 !== $password2) {
+                    echo 'Password does not match';
+                } else {
+                    // Hash the password
+                    $default_password = $password1;
+            
+                    // Start a transaction
+                    $conn->begin_transaction();
+            
+                    // Insert into user table
+                    $sql_user = "INSERT INTO user (firstName, middleName, lastName, contactNumber, zone, purok, dateJoined, userEmail, username, password) VALUES ('$first_name', '$middle_name', '$last_name', '$mobile_number', '$zone', '$purok', '$dateJoined', '$email', '$email', '$default_password')";
+            
+                    if ($conn->query($sql_user) === TRUE) {
+                        $conn->commit();
+                        header("Location: home.php");
+                    } else {
+                        // Rollback the transaction if there is an error in the first query
+                        $conn->rollback();
+                        echo 'Error: ' . $sql_user . '<br>' . $conn->error;
+                    }
+                }
+            }
+            
+            // Login form
+            if (isset($_POST['login'])) {
+                $email = $_POST['email'];
+                $password = $_POST['password'];
+            
+                // Prepare the MySQL query
+                $sql = "SELECT * FROM user WHERE userEmail = '$email'";
+                $result = $conn->query($sql);
+            
+                if ($result === false) {
+                    echo "Error executing the query: " . $conn->error;
+                    return;
+                }
+            
+                // Check if the user exists and the password is correct
+                if ($result->num_rows > 0) {
+                    $row = $result->fetch_assoc();
+                    if (password_verify($password, $row["password"])) {
+                        // Login successful, redirect to the home page
+                        header("Location: home.php");
+                        exit();
+                    } else {
+                        // Invalid password
+                        echo "Invalid oten.";
+                    }
+                } else {
+                    // User not found
+                     header("Location: home.php");;
+                }
+            }
+        
         }
-    }
 
-    // CREATE SEPERATE DATABASE TABLE FOR ADMIN BEFORE CREATING PHP FORM!!
-    ?>
-
-    </html>
-
+        
+        
+        ?>
+<?php ob_end_flush(); ?> 
 </body>
 
 </html>
