767375fe5237ebcc958425eaa2f34211146c036c
PainCake221 <99483109+PainCake221@users.noreply.github.com>
2024-04-24 14:55:40 +0000

    Minor Change

--- htdocs/admin/adminnav.php
+++ htdocs/admin/adminnav.php
@@ -2,7 +2,6 @@
 <html lang="en">
 
 <head>
-    <title>Admin Navigation</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
